a7c4161c097e9034f5c38e3553d11eee3f876dd7
Franco Bianco <franco.bianco93@gmail.com>
2022-08-31 13:42:20 -0300

    Readme & Minor changes

--- app/Http/Resources/Api/V1/CountryCollection.php
+++ app/Http/Resources/Api/V1/CountryCollection.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources\Api\V1;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class PainterCollection extends ResourceCollection
+class CountryCollection extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
